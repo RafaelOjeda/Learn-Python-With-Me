230e64794d4b119735ce82db4ef079e2000d0032
Rafael Ojeda <rafael.e.ojeda@gmail.com>
2020-08-28 01:28:03 -0400

    Added a README.md file. Updated Lists.py

--- Documentation for Beginners/Lists.py
+++ Documentation for Beginners/Lists.py
@@ -1,1 +1,39 @@
 # Lists store sets of information in one place.
+# Stored in a variable
+names = []
+
+# Currently the list is empty but I can add items using append()
+names.append("Rafael")
+names.append("Carlos")
+names.append("Ana")
+names.append("Rachel")
+
+# Shows the list names with all appeneded items 
+print(names)
+
+# Inserting adds items to a list in the index value that you wish. Shifting everything by one
+names.insert(0, "Donald")
+print(f"Adds {names[0]} to the list: {names}")
+
+# The del statements can remove a specific item within a list
+print(f"Deleted {names[0]}")
+del names[0]
+
+# The pop() method removes the last item in a list unless  the index value is stated within the method.
+remove_name = names.pop(3)
+print(f"User {remove_name} has been removed from the list")
+
+# The .remove() method can remove an item using the specific value that the item contains rather than the index value
+names.remove("Rafael")
+
+# All items can be accessed individually using their corresponding index value
+print(names[1]) # Prints second item in the names list
+
+# Used in a string
+print(f"Hello my name is {names[0]}.")
+
+# To change a value treat it like a variable and equal it to the value you wish to have.
+names[1] = "Miguel"
+
+print(f"An item has been changed to {names[1]}: {names}")
+
---
+++ README.md
@@ -0,0 +1,11 @@
+Hello everyone, 
+
+I am currently in college studying Computer Science. I am teaching myself Python mainly using the book called "Python Crash Course" by Eric Matthes. 
+
+This is what you would call my notebook for everything Python that I learn whether it be from the book or an online course. I also will have projects (possibly in another repository) that I do as part of the learning process. Something else that I will be learning is Git along with Markdown just to get used to this whole Github thing. My goal is to be able to make this a resource for everyone. Which is why I am asking for any person whether it be beginner or professonal programmer to pitch in (because I can't cover a whole language specially if I am learning as well). Teach everyone something you know and make this be like a dictionary for Python. 
+
+
+
+I am writting all of this using VSCode and will put any resources that I use to learn new things down below.
+
+Thank you for reading. If you'd like to contact me please email me at rafael.e.ojeda@gmail.com
